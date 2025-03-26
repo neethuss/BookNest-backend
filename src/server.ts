7ed282a358c8bdb8e455f1ad911501d0113e8711
neethuss <neethuanitha5@gmail.com>
2025-03-26 12:55:38 +0530

    fixing cors issue

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import connectDB from './config/dbConfig'
-import BookRoutes from './routes/BookRoute'
-import path from 'path'
-import { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/dbConfig';
+import BookRoutes from './routes/BookRoute';
+import path from 'path';
 
+dotenv.config();
 
-dotenv.config()
+connectDB();
 
-//database connection
-connectDB()
+const app = express();
+const port = process.env.PORT;
 
-const app = express()
-
-const port = process.env.PORT
 const allowedOrigins = [
   "https://book-nest-frontend-puce.vercel.app",
   "http://localhost:3000"
@@ -29,25 +26,21 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
-  // Handle preflight requests (OPTIONS method)
   if (req.method === "OPTIONS") {
-    res.status(200).end(); // Explicitly return a response
+    res.status(204).end();
     return;
   }
 
   next();
 });
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/', BookRoutes)
+app.use('/', BookRoutes);
 
-
-//port connection
 app.listen(port, () => {
-  console.log(`Backend server connected at port ${port}`)
-})
+  console.log(`Backend server connected at port ${port}`);
+});
