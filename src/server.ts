e6428dea1edd5b269484460feb082fd296ba794a
neethuss <neethuanitha5@gmail.com>
2025-03-26 15:11:07 +0530

    cors headers

--- src/server.ts
+++ src/server.ts
@@ -20,9 +20,11 @@ const corsOptions = {
     'http://localhost:3000' 
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   optionsSuccessStatus: 200
 };
+
 
 app.use(cors(corsOptions));
 
