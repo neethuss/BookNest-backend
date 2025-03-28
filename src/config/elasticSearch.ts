9505bee2ed8f303b109173bfc71e3dd7732a5037
neethuss <neethuanitha5@gmail.com>
2025-03-28 13:48:55 +0530

    elastic cloud

--- src/config/elasticSearch.ts
+++ src/config/elasticSearch.ts
@@ -1,18 +1,17 @@
 import { Client } from "@elastic/elasticsearch";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const elasticClient = new Client({
-  node: process.env.ELASTICSEARCH_NODE || "http://localhost:9200",
-  maxRetries: 5,
-  requestTimeout: 60000,
-  sniffOnStart: false,
+  cloud: {
+    id: process.env.ELASTICSEARCH_CLOUD_ID || "your_default_cloud_id"
+  },
   auth: {
-    username: 'elastic', // Default username for Elasticsearch
-    password: process.env.ELASTIC_PASSWORD || 'default_password' // Password from .env file
+    username: process.env.ELASTIC_USERNAME || "elastic",
+    password: process.env.ELASTIC_PASSWORD || "default_password"
   }
 });
+
 
 export default elasticClient;
